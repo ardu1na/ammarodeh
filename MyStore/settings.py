988b99a16856bc2a037876c755e3c49df4d7139c
ardu1na <arduinadelbosque@gmail.com>
2023-12-19 09:39:16 -0300

    fixed static files and root

--- MyStore/settings.py
+++ MyStore/settings.py
@@ -114,15 +114,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 # mystore/settings.py
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Structure/static'),
-    # Other static file directories if any
-]
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
