ce72cefd4712c6308cf2285fa0c3d5d75df6fc2a
ardu1na <arduinadelbosque@gmail.com>
2023-12-18 21:32:35 -0300

    starting migration of chart  app

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 app_name = 'products'
-
 urlpatterns = [
- path('<int:pk>/' , views.detail ,name='detail') ,
+    path('<int:pk>/' , views.detail ,name='detail') ,
+    path('product_list/', views.product_list, name="product_list"), 
+    path('cart/add/<int:product_id>/', views.add_product_into_cart, name="add_product"), 
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,18 +1,91 @@
-from django.shortcuts import render , get_object_or_404
-from .models import Products
+from django.shortcuts import render , get_object_or_404, redirect
+from .models import Products, \
+                    Cart, ProductCart
 
 def detail (request , pk) :
     product = get_object_or_404(Products , pk=pk)
     related_products = Products.objects.filter(category=product.category).exclude(pk=pk)[0:3]
-
-
-
 
     return render(request, 'Products/detail.html' , {
 
          'product' : product ,
          'related_products' : related_products ,
     })
+
+
 def product_list(request):
     products = Products.objects.all()  # Assuming you have a Product model
     return render(request, 'structure/test_page.html', {'products': products})
+
+
+
+############################################
+
+
+##########################
+################################## Cart and shopping logic
+
+#@api_view(['POST', 'GET'])
+#@authentication_classes([TokenAuthentication])
+#@permission_classes([IsAuthenticated])
+def add_product_into_cart(request, product_id):
+    client = request.user.client
+    cart, created = Cart.objects.get_or_create(client=client, done=False)
+    try:
+        product = Products.objects.get(pk=product_id)
+        product_cart, created = ProductCart.objects.get_or_create(cart=cart, product=product)
+        context = {
+            'cart': cart,
+            'cart_product': product_cart,
+            }
+        template_name = 'cart.html'    
+        return render(request, template_name, context)
+    
+    except Products.DoesNotExist:
+        context = {
+            'error': 'Ups! Something gets wrong, please try again or contact the webmaster. Thanks!'
+        }
+        return redirect('home')
+
+
+
+#@api_view(['DELETE'])
+#@authentication_classes([TokenAuthentication])
+#@permission_classes([IsAuthenticated])
+def delete_product_from_cart(request, product_id):
+    client = request.user.client
+    try:
+       # Delete the product from the cart
+        try:
+            product_cart = ProductCart.objects.get(pk=product_id)
+            cart = product_cart.cart
+            if cart.client == client:
+                product_cart.delete()
+        except ProductCart.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        cart_serializer = CartSerializer(instance=cart)
+        return Response({'cart': cart_serializer.data}, status=status.HTTP_200_OK)
+    
+    except Cart.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+
+
+#@api_view(['GET'])
+#@authentication_classes([TokenAuthentication])
+#@permission_classes([IsAuthenticated])
+def cart_detail(request):
+    client = request.user.client
+    try:
+        cart = Cart.objects.get(client=client, done=False)
+        cart_serializer = CartDetailSerializer(instance=cart)
+        return Response({'cart':cart_serializer.data}, status=status.HTTP_200_OK)
+    
+    except Cart.DoesNotExist:
+        cart = Cart.objects.create(client=client, done=False)
+        cart_serializer = CartDetailSerializer(instance=cart)
+        return Response({'cart':cart_serializer.data}, status=status.HTTP_200_OK)
+    
+
